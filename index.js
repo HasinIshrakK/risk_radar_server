7e73ec10ebed2949910ebe5288acb41ddbb08320
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-02-25 15:42:18 +0600

    High Amount Detection Backend is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,36 +11,48 @@ app.use(cors());
 app.use(express.json());
 
 // --- MongoDB Configuration ---
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rwnir9j.mongodb.net/risk_radar`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.w0nmtjl.mongodb.net/?appName=Cluster0`;
+
+
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 // --- Database Connection & Server Start ---
+
 async function run() {
-    try {
-        await client.connect();
-        
-        const db = client.db("risk_radar"); 
-        console.log("Successfully connected to MongoDB.");
+  try {
+    await client.connect();
 
-        // --- Routes ---
-        app.get('/', (req, res) => {
-            res.status(200).json({ message: 'RiskRadar API is healthy' });
-        });
+    const db = client.db("risk_radar");
+    const highAmountCollections = db.collection("high_amount");
+    
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port: ${PORT}`);
-        });
+    app.get("/high-amount", async (req, res) => {
+      const cursor = highAmountCollections.find();
+      const result =  await cursor.toArray();
+      res.send(result);
+    });
 
-    } catch (error) {
-        console.error("Failed to connect to the database:", error);
-    }
+
+    // ping
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+  }
 }
 
 run().catch(console.dir);
+
+// HOME
+app.get("/", (req, res) => res.send("Risk-Radar Backend Running!"));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
