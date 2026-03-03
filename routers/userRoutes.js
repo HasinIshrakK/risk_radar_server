c80df1711070efe3ce5410981638266dd9a52cc6
HasinIshrakK <hasinishrakk@gmail.com>
2026-03-03 13:03:39 +0600

    Route added for user geting

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get("/", userController.getUsers)
+router.get("/", userController.getUsers);
+router.get("/:id", userController.getUser);
 router.post("/", userController.createUser);
 router.patch("/:id", userController.updateUser);
 
