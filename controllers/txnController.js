53f7e5090d7d4df343981ba51c01b241d86bc5e4
Asha-08 <ashaamina91@gmail.com>
2026-03-10 13:37:46 +0600

    add midnight detection in txnController

--- controllers/txnController.js
+++ controllers/txnController.js
@@ -1,68 +1,92 @@
-const { redisClient } = require('../config/redis');
-const {getDB} = require("../config/db")
+const { redisClient } = require("../config/redis");
+const { getDB } = require("../config/db");
 
 exports.analyzeTransaction = async (req, res) => {
-    try {
-        const { userId, amount } = req.body;
+  try {
+    const { userId, amount, createdAt } = req.body;
 
-         if (!userId || !amount) {
-            return res.status(400).json({ message: "userId and amount required" });
-        }
-
-         const db = getDB();
-        const transactionCollection = db.collection("transaction");
-
-        const key = `rapid_tx:${userId}`;
-
-        // INCREMENT transaction count
-        const count = await redisClient.incr(key);
-
-        // If first transaction, set 5 min expiry (300 seconds)
-        if (count === 1) {
-            await redisClient.expire(key, 300);
-        }
-
-        // Risk Logic
-        let riskScore = count * 20;
-        let status = "SAFE";
-        let alert = false;
-        let reason = "";
-
-        if (count >= 3) {
-            status = "REVIEW_REQUIRED";
-            alert = true;
-             reason = "Multiple rapid transaction detected within 5 minutes";
-        }
-
-         // MongoDB Save
-        await transactionCollection.updateOne(
-            { userId },
-            {
-                $set: {
-                    lastAmount: amount,
-                    riskScore,
-                    status,
-                    alert,
-                    reason,
-                    lastUpdated: new Date(),
-                },
-                $inc: { totalTransaction: 1 },
-                $setOnInsert: { createdAt: new Date() }
-            },
-            { upsert: true }
-        );
-
-
-        res.status(200).json({
-            userId,
-            transactionCountLast5Min: count,
-            riskScore,
-            status,
-            alert,
-            reason,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Something went wrong" });
+    if (!userId || !amount) {
+      return res.status(400).json({ message: "userId and amount required" });
     }
+
+    const db = getDB();
+    const transactionCollection = db.collection("transaction");
+
+    const key = `rapid_tx:${userId}`;
+
+    // INCREMENT transaction count
+    const count = await redisClient.incr(key);
+
+    // If first transaction, set 5 min expiry (300 seconds)
+    if (count === 1) {
+      await redisClient.expire(key, 300);
+    }
+
+    // Risk Logic
+    let riskScore = count * 20;
+    let status = "SAFE";
+    let alert = false;
+    let reason = "";
+
+    // Midnight Detection
+    const hour = new Date(createdAt).getHours();
+
+    if (hour >= 0 && hour < 4) {
+      riskScore += 20;
+      alert = true;
+
+      if (reason) {
+        reason += " | ";
+      }
+
+      reason += "Transaction during midnight hours";
+    }
+
+    // Rapid Transaction Rule
+    if (count >= 3) {
+      alert = true;
+
+      if (reason) {
+        reason += " | ";
+      }
+
+      reason += "Multiple rapid transaction detected within 5 minutes";
+    }
+
+    // Final Risk Decision
+    if (riskScore >= 60) {
+      status = "REVIEW_REQUIRED";
+    }
+    
+
+    // MongoDB Save
+    await transactionCollection.updateOne(
+      { userId },
+      {
+        $set: {
+          lastAmount: amount,
+          riskScore,
+          status,
+          alert,
+          reason,
+          lastUpdated: new Date(),
+        },
+        $inc: { totalTransaction: 1 },
+        $setOnInsert: { createdAt: new Date() },
+      },
+      { upsert: true },
+    );
+
+    res.status(200).json({
+      userId,
+      transactionCountLast5Min: count,
+      riskScore,
+      status,
+      alert,
+      reason,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
