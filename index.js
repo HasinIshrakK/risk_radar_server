da10b15a94007c38b4efa4bfb13426d0693aba42
HasinIshrakK <hasinishrakk@gmail.com>
2026-03-12 10:11:15 +0600

    Revert "Merge pull request #10 from HasinIshrakK/mahfuz"
    This reverts commit 5d461b8966e769a620690a38526e336b8c4b8512, reversing
    changes made to 31d304cc5e5b55ddd4ca7f839b97c491455eccf8.

--- index.js
+++ index.js
@@ -1,60 +1,22 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = require("./app");
+const { connectDB } = require("./config/db");
+const { connectRedis } = require("./config/redis");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// --- Middleware ---
-app.use(cors());
-app.use(express.json());
-
-// --- MongoDB Configuration ---
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.w0nmtjl.mongodb.net/?appName=Cluster0`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-// --- Database Connection & Server Start ---
 async function run() {
-  try {
-    await client.connect();
-
-    const db = client.db("risk_radar");
-    const allUsersTransactionCollection = db.collection(
-      "all_users_transaction",
-    );
-
-    app.get("/users-transaction", async (req, res) => {
-      try {
-        const usersTransaction = await allUsersTransactionCollection
-          .find()
-          .toArray();
-        res.send(usersTransaction);
-      } catch (error) {
-        res.status(500).send({ error: "Failed to fetch transactions" });
-      }
-    });
-
-    console.log("Successfully connected to MongoDB.");
-
-    // --- Routes ---
-    app.get("/", (req, res) => {
-      res.status(200).json({ message: "RiskRadar API is healthy" });
-    });
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-  }
+    try {
+        await connectDB();
+        await connectRedis();
+        
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port: ${PORT}`);
+        });
+    } catch (err) {
+        console.error("Startup Error:", err);
+        process.exit(1);
+    }
 }
 
-run().catch(console.dir);
+run();
