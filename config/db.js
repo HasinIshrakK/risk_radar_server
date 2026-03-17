db9c80e03aa3af8b25334dfc7a6a96fdee2747ba
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-03-17 10:44:28 +0600

    backend

--- config/db.js
+++ config/db.js
@@ -1,27 +1,25 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rwnir9j.mongodb.net/risk_radar`;
+
 const client = new MongoClient(uri, {
-    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-// const connectDB = async () => {
-//     await client.connect();
-//     console.log("✅ MongoDB Connected");
-//     return client.db('risk_radar'); 
-// };
-
-// module.exports = { connectDB, client };
 
 
 let db;
 
 const connectDB = async () => {
-    await client.connect();
-    db = client.db('risk_radar');
-    console.log("✅ MongoDB Connected");
+  await client.connect();
+  db = client.db("risk_radar");
+  console.log("✅ MongoDB Connected");
 };
 
 const getDB = () => db;
 // add client in this line
-module.exports = { connectDB, getDB ,client};
+module.exports = { connectDB, getDB, client };
